5abe3a40e75e589f51a6b78ae49f3566b63ccc54
dmitriy_gruzdev <dmitriy_gruzdev@epam.com>
2016-10-05 22:20:47 +0300

    Shaders init in WebGLBase.

--- app/scripts/WebGLBase.js
+++ app/scripts/WebGLBase.js
@@ -2,8 +2,48 @@ export default class WebGLBase {
   constructor(document, canvasId) {
     this.gl = null;
     this.canvas = document.getElementById(canvasId);
+    this.vertexShader = null;
+    this.fragmentShader = null;
+    this.shaderProgram = null;
+    this.vertexPositionAttribute = null;
+    this.vertexShaderSource =
+      `
+      attribute vec3 aVertexPosition;
+      
+       uniform mat4 uMVMatrix;
+       uniform mat4 uPMatrix;
+  
+       void main(void) {
+          gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+        }`;
+    this.fragmentShaderSource =
+      `
+      void main(void) {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      }`;
 
     this._initWebGL();
+    this._initShaders();
+  }
+
+  _initVertexShader() {
+    this.vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+    this.gl.shaderSource(this.vertexShader, this.vertexShaderSource);
+    this.gl.compileShader(this.vertexShader);
+
+    if (!this.gl.getShaderParameter(this.vertexShader, this.gl.COMPILE_STATUS)) {
+      console.error('Vertex shader hasn\'t been initialized');
+    }
+  }
+
+  _initFragmentShader() {
+    this.fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+    this.gl.shaderSource(this.fragmentShader, this.fragmentShaderSource);
+    this.gl.compileShader(this.fragmentShader);
+
+    if (!this.gl.getShaderParameter(this.fragmentShader, this.gl.COMPILE_STATUS)) {
+      console.error('Fragment shader hasn\'t been initialized');
+    }
   }
 
   _initWebGL() {
@@ -11,6 +51,26 @@ export default class WebGLBase {
       this.gl = this.canvas.getContext('webgl') ||
         this.canvas.getContext('experimental-webgl');
     }
+  }
+
+  _initShaders() {
+    this._initFragmentShader();
+    this._initVertexShader();
+
+    this.shaderProgram = this.gl.createProgram();
+    this.gl.attachShader(this.shaderProgram, this.vertexShader);
+    this.gl.attachShader(this.shaderProgram, this.fragmentShader);
+    this.gl.linkProgram(this.shaderProgram);
+
+    if (!this.gl.getProgramParameter(this.shaderProgram, this.gl.LINK_STATUS)) {
+      console.error("Unable to initialize the shader program.");
+      return;
+    }
+
+    this.gl.useProgram(this.shaderProgram);
+
+    this.vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
+    this.gl.enableVertexAttribArray(this.vertexPositionAttribute);
   }
 }
 
